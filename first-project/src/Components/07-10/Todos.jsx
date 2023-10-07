6a377304f9bcad5d334e83d519ad3ee2b9c6899e
yogi2456 <138296327+yogi2456@users.noreply.github.com>
2023-10-08 01:04:51 +0530

    updates

--- first-project/src/Components/07-10/Todos.jsx
+++ first-project/src/Components/07-10/Todos.jsx
@@ -1,11 +1,11 @@
-import React, {memo} from 'react';
+import React, { memo } from 'react'
 
-const Todos = ({todos, addTodo}) => {
-    alert("Alert inside from todos component..")
+const Todos = ({ todos, addTodo }) => {
+    alert("Alert inside from todos component.")
     return (
         <div>
             <h1>Todos</h1>
-            <button onClick={addTodo}>Add Todo</button>
+            <button onClick={addTodo}>Add todo</button>
             {todos.map((todo) => (
                 <h1>{todo}</h1>
             ))}
--- first-project/src/Components/07-10/UseCallback.jsx
+++ first-project/src/Components/07-10/UseCallback.jsx
@@ -1,25 +1,25 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useState } from 'react'
 import Todos from './Todos'
 
 const UseCallback = () => {
+    const [counter, setCounter] = useState(0)
+    const [todos, setTodos] = useState([]);
 
-    const [counter, setCounter] = useState(0);
-    const [todo, setTodos] = useState([]);
-
-    function addCounter () {
-         setCounter((value) => value + 1)
+    function addCounter() {
+        setCounter((value) => value + 1)
     }
 
-    const addTodo = useCallback (() => {
-        setTodos ([...todo, "NewTodo"])
-    }, [todo])
+    const addTodo = useCallback(() => {
+        setTodos([...todos, "New todo"])
+    }, [todos])
+
     return (
         <div>
             <h1>Counter - {counter}</h1>
             <button onClick={addCounter}>+</button>
-            <Todos  todo={todo} addTodo={addTodo} />
+            <Todos todos={todos} addTodo={addTodo} />
         </div>
     )
 }
 
-export default UseCallback;
+export default UseCallback
