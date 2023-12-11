caf19429105dfc75a86c1d60fa126845fca1ff6f
yogi2456 <138296327+yogi2456@users.noreply.github.com>
2023-12-11 16:05:00 +0530

    updates

--- first-project/src/helpers/Axios.Config.js
+++ first-project/src/helpers/Axios.Config.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-var BackendUrl = "http://localhost:8000/api/v1"
-if (process.env.MODE === "PRODUCTION") {
-    BackendUrl = "https://backend-practice-4.onrender.com/api/v1"
+// var BackendUrl = "http://localhost:8000/api/v1"
+// if (process.env.MODE === "PRODUCTION") {
+  var BackendUrl = "https://backend-practice-4.onrender.com/api/v1"
     //BackendUrl = "https://awdiz-backend-4.onrender.com/api/v1"
-}
+//}
 
 const api = axios.create({ baseURL: BackendUrl })
 
